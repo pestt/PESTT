8561061b74db91e17f31f03e1afbdde6eed28668
Rui Gameiro <rrui.gameiro@gmail.com>
2012-03-10 09:53:40 +0000

    RenumVisitor fixed.

--- PESTT/src/domain/graph/visitors/RenumNodesGraphVisitor.java
+++ PESTT/src/domain/graph/visitors/RenumNodesGraphVisitor.java
@@ -1,14 +1,24 @@
 package domain.graph.visitors;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import adt.graph.Node;
 
 public class RenumNodesGraphVisitor extends DepthFirstGraphVisitor<Integer> {
 
 	private int nodeNum = 0;
+	private Set<Node<Integer>> visitedNodes; // nodes must be visited just one
+												// time
+
+	public RenumNodesGraphVisitor() {
+		visitedNodes = new HashSet<Node<Integer>>();
+	}
 
 	@Override
 	public boolean visit(Node<Integer> node) {
-		if(super.visit(node)) {
+		if(!visitedNodes.contains(node)) {
+			visitedNodes.add(node);
 			node.setValue(nodeNum);
 			nodeNum++;
 			return true;
