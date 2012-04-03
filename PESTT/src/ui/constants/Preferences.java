2b282d4c709bfeb0519f1542f7f7adcb28ef1e37
Rui Gameiro <rrui.gameiro@gmail.com>
2012-04-03 13:04:29 +0100

    Update site

--- PESTT/src/ui/constants/Preferences.java
+++ PESTT/src/ui/constants/Preferences.java
@@ -5,6 +5,6 @@ public class Preferences {
 	public static final String DESCRIPTION = "PESTT preferrences:\n";
 	public static final String DOT_PATH = "pathPreference";
 	public static final String DOT_PATH_LABEL = "Dot location:";
-	public static final String SUPPORT_PAGE = "\n\nAll necessary support <a href=\"https://github.com/pestt\">here</a>.";          
+	public static final String SUPPORT_PAGE = "\n\nAll necessary support <a href=\"http://pestt.github.com/\">here</a>.";          
 
 }
