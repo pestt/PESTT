d3513f0f7e817640d80b9953d86ee37c9e42b051
Rui Gameiro <rrui.gameiro@gmail.com>
2012-04-26 12:42:15 +0100

    Add Byteman runner class

---
+++ PESTT/src/domain/coverage/instrument/BytemanRunner.java
@@ -0,0 +1,31 @@
+package domain.coverage.instrument;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import main.activator.Activator;
+
+import org.eclipse.jface.preference.IPreferenceStore;
+
+import ui.constants.Preferences;
+
+public class BytemanRunner {
+	
+	public void run(String path, String className, String classPath) {
+		IPreferenceStore preferenceStore = Activator.getDefault().getPreferenceStore();
+		String byteman = preferenceStore.getString(Preferences.BYTEMAN_PATH);
+		String cmd = "java -javaagent:" + byteman + "/lib/byteman.jar=script:" + path + " -cp " + classPath + " " + className;
+		try {
+			Process p = Runtime.getRuntime().exec(cmd);  
+	        BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));  
+	        String line = null;  
+	        while ((line = in.readLine()) != null) {  
+	            System.out.println(line);  
+	        }  
+	    } catch (IOException e) {  
+	        e.printStackTrace();  
+	    }  
+	}
+
+}
