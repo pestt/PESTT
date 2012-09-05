49cc3c7d7f6d858f47d88497cb6b9bcb48fde6a3
Rui Gameiro <rrui.gameiro@gmail.com>
2012-09-05 11:28:32 +0100

    Executed Paths Bug Fixed.

--- PESTT/src/domain/constants/Byteman.java
+++ PESTT/src/domain/constants/Byteman.java
@@ -9,5 +9,5 @@ public class Byteman {
 	public static final String SCRIPT_DIR = System.getProperty("java.io.tmpdir") + IPath.SEPARATOR + "script";
 	public static final String SCRIPT_FILE = "rules.btm";
 	public static final String OUTPUT_FILE = "output.txt";
-	public static final String HELPER_LOCATIO = "domain.coverage.instrument.HelperClass";
+	public static final String HELPER_LOCATIO = "domain.tests.instrument.HelperClass";
 }
